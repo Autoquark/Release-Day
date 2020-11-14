6ea31f4b1fa1b492c809e0ef5373f691b3d294ae
Autoquark <github@autoquark.33mail.com>
2020-11-14 16:52:00 +0000

    Camera follows player

---
+++ Assets/Common/RectEx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Common
+{
+    static class RectEx
+    {
+        public static Rect FromCorners(Vector2 one, Vector2 two) => new Rect(Mathf.Min(one.x, two.x), Mathf.Min(one.y, two.y), Mathf.Abs(one.x - two.x), Mathf.Abs(one.y - two.y));
+
+        public static Rect FromCentreSize(Vector2 centre, Vector2 size) => new Rect(centre - size / 2, size);
+    }
+}
---
+++ Assets/Extensions/TransformExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Extensions
+{
+    static class TransformExtensions
+    {
+        public static IEnumerable<Transform> Children(this Transform transform) => transform.Cast<Transform>();
+    }
+}
