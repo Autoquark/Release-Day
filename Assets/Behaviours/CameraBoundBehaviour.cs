2fb3ef842126c4206541174a818050325952cea8
Autoquark <github@autoquark.33mail.com>
2020-11-17 13:10:54 +0000

    Add mechanism to limit where camera can show

---
+++ Assets/Behaviours/CameraBoundBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Behaviours
+{
+    class CameraBoundBehaviour : MonoBehaviour
+    {
+        private const float gizmoSize = 0.5f;
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawLine(transform.position + Vector3.left * gizmoSize, transform.position + Vector3.right * gizmoSize);
+            Gizmos.DrawLine(transform.position + Vector3.up * gizmoSize, transform.position + Vector3.down * gizmoSize);
+        }
+    }
+}
