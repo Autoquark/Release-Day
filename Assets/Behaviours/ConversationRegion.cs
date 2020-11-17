bd2fd2d3cbb2c737a102f5aa95445bcf79b52f81
Autoquark <github@autoquark.33mail.com>
2020-11-17 18:52:47 +0000

    Require grounded option for conversation trigger

--- Assets/Behaviours/ConversationRegion.cs
+++ Assets/Behaviours/ConversationRegion.cs
@@ -9,6 +9,8 @@ namespace Assets.Behaviours
 {
     class ConversationRegion : MonoBehaviour
     {
+        public bool _requireGrounded = true;
+
         class Info
         {
             public bool hasTriggered = false;
@@ -18,8 +20,6 @@ namespace Assets.Behaviours
         private Lazy<ConversationController> _conversationController;
         private Lazy<ConversationLoader> _loader;
 
-        private bool _alreadyTriggered = false;
-
         public ConversationRegion()
         {
             _playerCollider = new Lazy<Collider2D>(() => FindObjectOfType<PlayerControllerBehaviour>().GetComponent<Collider2D>());
@@ -27,13 +27,13 @@ namespace Assets.Behaviours
             _loader = new Lazy<ConversationLoader>(GetComponent<ConversationLoader>);
         }
 
-        private void OnTriggerEnter2D(Collider2D collision)
+        private void OnTriggerStay2D(Collider2D collision)
         {
             var data = LevelDataStore.GetOrCreate<Info>(gameObject.name);
             if (data.hasTriggered)
                 return;
 
-            if (collision == _playerCollider.Value)
+            if (collision == _playerCollider.Value && (!_requireGrounded || collision.GetComponent<PhysicsObject>().Grounded))
             {
                 _conversationController.Value.SetConversation(_loader.Value.Conversation);
                 _conversationController.Value.SetVisibility(true);
