92fcf33ac04c9eb0c66336ee0e5803092d368cc6
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2020-11-17 16:28:25 +0000

    Basic spikes, teleporter placeholder sprite

---
+++ Assets/Behaviours/SpikesBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Assets.Data;
+
+namespace Assets.Behaviours
+{
+    class SpikesBehaviour : MonoBehaviour
+    {
+        private Lazy<Collider2D> _collider;
+
+        SpikesBehaviour()
+        {
+        }
+
+        private void OnTriggerEnter2D(Collider2D hitBy)
+        {
+            var player = hitBy.GetComponent<PlayerControllerBehaviour>();
+
+            if (!player)
+                return;
+
+            GameObject.Destroy(player.gameObject);
+        }
+    }
+}
