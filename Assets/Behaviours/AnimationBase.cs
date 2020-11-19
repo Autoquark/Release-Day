acba98e056b22702c0129d9e1eea7c262ad7690a
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2020-11-19 11:45:23 +0000

    Turret animation

---
+++ Assets/Behaviours/AnimationBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Assets.Data;
+using UnityEngine.Tilemaps;
+using Spine.Unity;
+using Animation = Spine.Animation;
+
+namespace Assets.Behaviours
+{
+
+    class AnimationBase : MonoBehaviour
+    {
+        protected readonly Lazy<SkeletonAnimation> _skeletonAnimation;
+
+        protected AnimationBase()
+        {
+            _skeletonAnimation = new Lazy<SkeletonAnimation>(GetComponent<SkeletonAnimation>);
+        }
+
+        protected void SetAnimationIfDifferent(Animation animation)
+        {
+            if (_skeletonAnimation.Value == null || _skeletonAnimation.Value.state == null)
+            {
+                return;
+            }
+
+            if (_skeletonAnimation.Value.state.GetCurrent(0)?.Animation == animation)
+            {
+                return;
+            }
+
+            _skeletonAnimation.Value.state.SetAnimation(0, animation, true);
+            _skeletonAnimation.Value.AnimationState.TimeScale = 1;
+        }
+    }
+}
