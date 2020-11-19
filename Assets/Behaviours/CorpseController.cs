8397efe0195be055f4f33842db27681ae6e4e774
Ian Badcoe <ian_badcoe@yahoo.co.uk>
2020-11-19 10:11:27 +0000

    Destroy corpses that fall out of the world...

---
+++ Assets/Behaviours/CorpseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Assets.Data;
+using UnityEngine.Tilemaps;
+
+namespace Assets.Behaviours
+{
+
+    class CorpseController : MonoBehaviour
+    {
+        private readonly Lazy<Tilemap> _tileMap;
+
+        CorpseController()
+        {
+            _tileMap = new Lazy<Tilemap>(FindObjectOfType<Tilemap>);
+        }
+
+        private void Update()
+        {
+            if (_tileMap.Value.localBounds.SqrDistance(transform.position) > 400)
+            {
+                GameObject.Destroy(gameObject);
+            }
+        }
+    }
+}
